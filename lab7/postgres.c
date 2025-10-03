4d157aa3aa38dbd3463e2e1dfb16af026963710f
Teo Podobnik <teo.podobnik1234@gmail.com>
2025-10-03 12:41:15 +0000

    focused on SIMPLE and EXTENDED QUERY

--- lab7/postgres.c
+++ lab7/postgres.c
@@ -4,7 +4,7 @@
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
-// Instead of allocating on bpf stack, we allocate on a per-CPU array map due to BPF stack limit of 512 bytes
+// Instead of allocating on bpf stack, we allocate on a per-CPU array map due to BPF stack limit
 struct {
      __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
      __type(key, __u32);
@@ -55,17 +55,15 @@ int process_enter_of_syscalls_write(__u64 fd, char* buf, __u64 payload_size){
 
     // Check if the L7 protocol is Postgres otherwise set to unknown
     req->protocol = PROTOCOL_UNKNOWN;
-    req->method = METHOD_UNKNOWN;
     req->request_type = 0;
     if (buf) {
         if (parse_client_postgres_data(buf, payload_size, &req->request_type)) {
-            bpf_printk("Client request type: %c\n", req->request_type);
-            if (req->request_type == POSTGRES_MESSAGE_TERMINATE){
-                req->protocol = PROTOCOL_POSTGRES;
-                req->method = METHOD_STATEMENT_CLOSE_OR_CONN_TERMINATE;
-            }
+            bpf_printk("Client request type: %c", req->request_type);
             req->protocol = PROTOCOL_POSTGRES;
-        }
+        } else {
+	    // Not recognizing client request as PostgreSQL protocol
+	    return 0;
+	}
     }
 
     // Copy the payload from the packet and check whether it fit below the MAX_PAYLOAD_SIZE
@@ -124,7 +122,7 @@ int process_exit_of_syscalls_read(void* ctx, __s64 ret) {
 
     // Retrieve the socket to which the client wrote
     // We can afford that since this eBPF program is meant for single-node environments
-    // Where of our interest are only the clients on current node that write to the PostgreSQL running on the same node 
+    // Where of our interest are only the clients on current node that write to the PostgreSQL (running on the same node)
     struct socket_key k = {};
     k.pid = pid;
     k.fd = read_info->fd;
@@ -144,37 +142,24 @@ int process_exit_of_syscalls_read(void* ctx, __s64 ret) {
     }
     e->fd = k.fd;
     e->pid = k.pid;
-    e->method = active_req->method;
     e->protocol = active_req->protocol;
     e->payload_size = active_req->payload_size;
     e->payload_read_complete = active_req->payload_read_complete;
+    e->request_type = active_req->request_type;
     bpf_probe_read(e->payload, MAX_PAYLOAD_SIZE, active_req->payload);
-
-    if (read_info->buf) {
-        if (e->protocol == PROTOCOL_POSTGRES) {
-            int ret = parse_postgres_server_resp(read_info->buf, ret);
-            if (active_req->request_type == POSTGRES_MESSAGE_SIMPLE_QUERY) {
-                e->method = METHOD_SIMPLE_QUERY;
-                bpf_printk("Simple Query read on the Server\n");
-            } else if (active_req->request_type == POSTGRES_MESSAGE_PARSE || active_req->request_type == POSTGRES_MESSAGE_BIND) {
-                e->method = METHOD_EXTENDED_QUERY;
-                bpf_printk("Extended Query read on the Server\n");
-            }
-        }
-    } else {
-        bpf_map_delete_elem(&active_reads, &id);
-        return 0;
-    }
 
     // All data is now stored in the L7 Event and we can clean up the structs in the eBPF maps
     bpf_map_delete_elem(&active_reads, &id);
     bpf_map_delete_elem(&active_l7_requests, &k);
 
     // Forward L7 event to userspace application
+    bpf_printk("Sending out event for request type %d", active_req->request_type);
     long r = bpf_perf_event_output(ctx, &l7_events, BPF_F_CURRENT_CPU, e, sizeof(*e));
     if (r < 0) {
-        bpf_printk("failed write to l7_events");     
+        bpf_printk("Failed write to l7_events");     
     }
+
+    bpf_printk("\n");
 
     return 0;
 }
